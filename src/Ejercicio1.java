9bf49e8c9ff962ea992d62d56f04cba5c1952afd
Leidy Bravo <leidymaireth5555@gmail.com>
2026-08-21 18:13:01 -0500

    Solucion del ejercicio 1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,14 @@
+public class Ejercicio1 {
+
+    //Ejercicio 1 · Metodo void sin parámetros BÁSICO
+    //Crea un metodo mostrarBienvenida() que imprima un mensaje de bienvenida
+    // del instituto en consola. Llámalo desde main .
+    //public static void mostrarBienvenida()
+
+    public static void main(String[] args) {
+        mostrarBienvenida();
+    }
+    public static void mostrarBienvenida(){
+        System.out.println("Bienvenidos al Instituto");
+    }
+}
